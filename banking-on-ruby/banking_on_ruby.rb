29376b8de9ca49167370bc86674d9d42eceb48e8
Physolia <nomi.bunes@gmail.com>
2022-04-02 14:47:56 -0700

    Updated names and balances to detangle text

--- banking-on-ruby/banking_on_ruby.rb
+++ banking-on-ruby/banking_on_ruby.rb
@@ -7,7 +7,7 @@ class Account
   
     public
     def display_balance(pin_number)
-      puts pin_number == pin ? "Balance: $#{@balance}." : pin_error
+      puts pin_number == pin ? " \n\ Name:    #{@name} \n\ Balance: $#{@balance}" : pin_error
     end
     
     def withdraw(pin_number, amount)
@@ -16,7 +16,7 @@ class Account
           @balance -= amount
           puts "Withdrew #{amount}. New balance: $#{@balance}."
         else
-          puts "Insufficient funds: $#{@balance}."
+          puts "Insufficient funds: $#{@balance} for #{@name}."
         end
       else
         puts pin_error
@@ -26,17 +26,16 @@ class Account
     def deposit(pin_number, amount)
       if pin_number == @pin
         @balance += amount
-        puts "Deposited #{amount}. New balance: $#{@balance}."
+        puts " \n\ #{@name} has deposited $#{amount}. \n\ New Balance: $#{@balance}"
       else
         puts pin_error
       end
     end
-
   
     private
 
     def pin
-      @pin = 3426
+      @pin = 2436
     end
 
     def pin_error
@@ -45,47 +44,55 @@ class Account
   end
 
 save_account = Account.new("Harry Blackstone Inverse Copperfield Dresden", 10_311_972)
-save_account.display_balance(3426)
-save_account.deposit(3426, 2_200_000_000)
+save_account.display_balance(2436)
+save_account.deposit(2436, 2_200_000_000)
 
 backup_cousin_account = Account.new("Dylan Diamond Inverse Dayton Bradshaw", 1_071_982)
-backup_cousin_account.display_balance(3426)
-backup_cousin_account.deposit(3426, 5_051_992)
-backup_cousin_account.display_balance(3426)
-backup_cousin_account.deposit(3426, 2_200_000_000)
+backup_cousin_account.display_balance(2436)
+backup_cousin_account.deposit(2436, 5_051_992)
+backup_cousin_account.display_balance(2436)
+backup_cousin_account.deposit(2436, 2_200_000_000)
 
 old_account = Account.new("Joel Martin Nomi Ellie Inverse", 10_151_985)
-old_account.display_balance(3426)
-old_account.deposit(3426, 2_200_000_000)
+old_account.display_balance(2436)
+old_account.deposit(2436, 2_200_000_000)
 
-rin_account = Account.new("Violet Emerald Inverse Starash", 7_311_998)
-rin_account.display_balance(3426)
-rin_account.deposit(3426, 2_200_000_000)
+joel_account = Account.new("Violet Emerald Inverse Starash", 7_311_998)
+joel_account.display_balance(2436)
+joel_account.deposit(2436, 36_262_462_002)
 
-my_account = Account.new("Rin Cyan Ruby Inverse Stardust Higgsfield ", 11_131_998)
-my_account.display_balance(3426)
-my_account.deposit(3426, 2_200_000_000)
+my_rin_account = Account.new("Rin Cyan Ruby Inverse Stardust Higgsfield", 11_131_998)
+my_rin_account.display_balance(2436)
+my_rin_account.deposit(2436, 36_462_261_998)
 
 molly_account = Account.new("Molly Leahy Inverse Stark Carpenter", 10_132_000)
-molly_account.display_balance(3426)
-molly_account.deposit(3426, 2_200_000_000)
+molly_account.display_balance(2436)
+molly_account.deposit(2436, 2_236_262_002)
 
 coden_account = Account.new("Coden Aaron Inverse Carpenter Stark", 7_302_002)
-coden_account.display_balance(3426)
-coden_account.deposit(3426, 2_200_000_000)
+coden_account.display_balance(2436)
+coden_account.deposit(2436, 2_236_261_998)
 
 maggie_account = Account.new("Maggie Bonnie Inverse Lefay Dresden", 3_312_011)
-maggie_account.display_balance(3426)
-maggie_account.deposit(3426, 2_200_000_000)
+maggie_account.display_balance(2436)
+maggie_account.deposit(2436, 2_224_261_992)
 
-bruce_account = Account.new("Bruce Thomas Inverse Banner Wayne ", 5_312_018)
-bruce_account.display_balance(3426)
-bruce_account.deposit(3426, 2_200_000_000)
+bruce_account = Account.new("Bruce Thomas Inverse Banner Wayne", 5_312_018)
+bruce_account.display_balance(2436)
+bruce_account.deposit(2436, 46_362_261_983)
 
 barry_account = Account.new("Barry Iris Inverse West Allen", 8_312_021)
-barry_account.display_balance(3426)
-barry_account.deposit(3426, 2_200_000_000)
+barry_account.display_balance(2436)
+barry_account.deposit(2436, 6_206_101_986)
 
 clark_account = Account.new("Clark Joseph Inverse Shoreline Kent", 1_312_022)
-clark_account.display_balance(3426)
-clark_account.deposit(3426, 2_200_000_000)
+clark_account.display_balance(2436)
+clark_account.deposit(2436, 2_233_101_988)
+
+jordan_account = Account.new("Jordan Johnathan Inverse Shoreline Kent", 3_312_022)
+jordan_account.display_balance(2436)
+jordan_account.deposit(2436, 7_314_261_998)
+
+naomi_account = Account.new("Naomi Inverse Stardust Higgsfield McDuffie", 7_312_022)
+naomi_account.display_balance(2436)
+naomi_account.deposit(2436, 11_134_261_998)
